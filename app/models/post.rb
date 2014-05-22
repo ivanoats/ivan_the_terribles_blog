9ebeaada91e722083a9be5d6fea81c5032ab801e
Martin Nash <martin.j.nash@gmail.com>
2014-05-21 22:42:52 -0700

    Added method to cache and return all Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
   has_many :comments
+
+  after_commit :invalidate_all_cache_key
+
+  @@all_cache_key = Time.now.to_s
+
+  def invalidate_all_cache_key
+    @@all_cache_key = Time.now.to_s
+  end
+
+  def self.cached_all
+    Rails.cache.fetch([self, @@all_cache_key]) { self.all }
+  end
+
+
 end
